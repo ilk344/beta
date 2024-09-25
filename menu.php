35b472d120996a6203a96c9ad1d03aec33926998
Dim <danna.hernandez.alp@cbtis258.edu.mx>
2024-09-25 11:07:33 -0600

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Menu</title>
+</head>
+<body><br><br><br><br><center>
+<button><a href="menu2.php">Ingresar Empresa</a></button>
+<button><a href="mostrador.php">Mostrador de Empresas</a></button>
+</center>
+</body>
+</html>
+
---
+++ menu2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Menu</title>
+</head>
+<body><br><br><br><br><center>
+	<form method="POST" action="altas.php">
+	<input type="password" placeholder="Password" required required pattern="DGEti123"><br><br>
+<input type="submit" value="Empresa con Sistema Dual">
+</form><br><br><br><br>
+<form method="POST" action="mostrador.php">
+	<input type="password" placeholder="Password" required required pattern="DGEti123"><br><br>
+<input type="submit" value="Empresa disponible para prácticas">
+</form>
+<br><br><br><br>
+<form method="POST" action="menu.php">
+	<input type="password" placeholder="Password" required required pattern="DGEti123"><br><br>
+<input type="submit" value="Empresa disponible para ambos modelos">
+</form>
+
+</center>
+</body>
+</html>
+
